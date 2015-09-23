55cba6a93fe7e275170c1edb273cd3034d4e8d9e
Rutger Farry <rutgerfarry@gmail.com>
2015-09-23 13:36:10 -0700

    Now inserting properly-formatted data on first run
    I know its a horrible crime to put so much in one commit, but oh well. Currently finishing testing rawCourseJSONFetcher, then it will be wrapped back up in scraper.js. I changed my philosophy from just inserting all the JSON to getting all the data, but now nonredundant and formatted.

--- rawCourseJSONFetcher.js
+++ rawCourseJSONFetcher.js
@@ -4,65 +4,202 @@
  * Fetches JSON from the OSU Course Catalog API and saves it to RethinkDB
  */
 
-const r         = require('rethinkdb');
 const request   = require('request');
 const async     = require('async');
+const thinky    = require('./thinky');
+const moment    = require('moment');
+
+const Course    = require('./Course');
+const Section   = require('./Section');
 
 const CATALOG_API =
 'http://catalog.oregonstate.edu/Services/CatalogService.svc/rest';
-const COURSE_ENDPOINT =
-CATALOG_API + '/courses/all';
+const COURSE_ENDPOINT = CATALOG_API + '/courses/all';
+const SECTION_ENDPOINT = CATALOG_API + '/course';
 
-let connection;
+function downloadCourseJSON(callback) {
+  console.log('Downloading course catalog from ' + COURSE_ENDPOINT);
+
+  request.get(COURSE_ENDPOINT, function handleResponse(error, res, body) {
+    if (res.statusCode !== 200) {
+      err = new Error('Got response code: ' +
+        res.statusCode +
+        ' from server');
+      callback(err);
+    } else if (error) {
+      callback(error);
+    } else {
+      const courseJSON = JSON.parse(body).slice(1, 50);
+      callback(null, courseJSON);
+    }
+  });
+}
+
+function courseSchemasFromCourseJSON(coursesJSON, callback) {
+  console.log('Creating Course Schemas');
+
+  const courses = coursesJSON.map((courseJSON) => {
+    let prereqs = [];
+    if (typeof courseJSON.CoursePrereqs !== 'undefined') {
+      prereqs = courseJSON.CoursePrereqs.map((prereqJSON) => {
+        return {
+          subjectCode: prereqJSON.SubjectCode,
+          courseNumber: prereqJSON.CourseNumber,
+        };
+      });
+    }
+
+    const creditArray = [];
+    if (typeof courseJSON.CreditLow !== 'undefined') {
+      creditArray.push(courseJSON.CreditLow);
+    }
+    if (typeof courseJSON.CreditHigh !== 'undefined') {
+      creditArray.push(courseJSON.CreditHigh);
+    }
+
+    const course = new Course({
+      title: courseJSON.Title,
+      subjectCode: courseJSON.SubjectCode,
+      courseNumber: courseJSON.CourseNumber,
+      credits: creditArray,
+      description: courseJSON.Description,
+      prereqs: prereqs,
+      dateScraped: Date.now(),
+    });
+
+    return course;
+  });
+
+  callback(null, courses);
+}
+
+function downloadSectionJSON(courseSchemas, callback) {
+  console.log('Downloading section JSON');
+  let sectionsJSON = [];
+  async.each(
+    courseSchemas,
+    function getCourse(course, innerCallback) {
+      console.log('Downloading course: ' +
+        course.subjectCode +
+        ' ' +
+        course.courseNumber
+      );
+      const ref = '/' + course.subjectCode + '/' + course.courseNumber;
+      const fullRef = SECTION_ENDPOINT + ref;
+      request.get(fullRef, function handleResponse(error, res, body) {
+        if (error) {
+          innerCallback(error);
+        } else if (res.statusCode !== 200) {
+          const err = new Error('Got response code: ' +
+            res.statusCode +
+            ' from server');
+          innerCallback(err);
+        } else {
+          sectionsJSON = JSON.parse(body).Offerings;
+          course.sections = sectionSchemasFromSectionJSON(sectionsJSON);
+          innerCallback();
+        }
+      });
+    },
+    function finishedGettingSections(err) {
+      callback(err, courseSchemas, sectionsJSON);
+    });
+}
+
+function insertCourses(courses, callback) {
+    // saves each course, calls callback when finished
+    console.log('Inserting ' + courses.length + ' courses into RethinkDB');
+    async.each(
+      courses,
+      function insertCourse(course, innerCallback) {
+        try {
+          course.validate();
+        } catch (err) {
+          innerCallback(null);
+          return;
+        }
+
+        course.saveAll(function doneSaving(err) {
+          if (err) {
+            console.error(err);
+          }
+          innerCallback(null);
+        });
+      },
+      callback
+    );
+  }
+
+function sectionMeetingTimesFromMeetingTimeJSONArray(meetingTimeJSONArray) {
+  if (typeof meetingTimeJSONArray === 'undefined') {
+    return null;
+  }
+  const meetingTimeArray = meetingTimeJSONArray.map((meetingTimeJSON) => {
+    return {
+      startTime: moment(meetingTimeJSON.BeginTime, 'HHmm').format() || null,
+      endTime: moment(meetingTimeJSON.EndTime, 'HHmm').format() || null,
+      days: meetingTimeJSON.DaysOfTheWeek || null,
+      buildingCode: meetingTimeJSON.BuildingCode || null,
+      roomNumber: meetingTimeJSON.RoomCode || null,
+    };
+  });
+  return meetingTimeArray;
+}
+
+function sectionSchemasFromSectionJSON(sectionsJSON) {
+  if (typeof sectionsJSON === 'undefined') {
+    return null;
+  }
+  const sections = sectionsJSON.map(function createSection(sectionJSON) {
+    const creditArray = [];
+    if (typeof sectionJSON.CreditLow !== 'undefined') {
+      creditArray.push(sectionJSON.CreditLow);
+    }
+    if (typeof sectionJSON.CreditHigh !== 'undefined') {
+      creditArray.push(sectionJSON.CreditHigh);
+    }
+    function calculateCurrent(maximum, available) {
+      const current = parseInt(maximum, 10) - parseInt(available, 10);
+      return (isNaN(current) || current < 0) ? 0 : current;
+    }
+
+    return new Section({
+      term: sectionJSON.TermShortDescription,
+      session: sectionJSON.Session,
+      crn: sectionJSON.CRN,
+      credits: creditArray,
+      instructor: sectionJSON.instructortor,
+      meetingTimes: sectionMeetingTimesFromMeetingTimeJSONArray(
+        sectionJSON.MeetingTimes
+      ),
+      startDate: moment(sectionJSON.StartDate).format(),
+      endDate: moment(sectionJSON.StartDate).format(),
+      campus: sectionJSON.Campus,
+      type: sectionJSON.ScheduleType,
+      status: sectionJSON.Status,
+      capacity: sectionJSON.MaxEnrollment,
+      currentEnrollment: calculateCurrent(sectionJSON.MaxEnrollment, sectionJSON.AvailableEnrollment),
+      waitlistCapacity: sectionJSON.MaxWait,
+      waitlistCurrent: calculateCurrent(sectionJSON.MaxWait, sectionJSON.AvailableWait),
+      fees: null,
+      restrictions: null,
+      comments: sectionJSON.Comments,
+    });
+  });
+  return sections;
+}
 
 async.waterfall([
-  function connectToRethink(callback) {
-    console.log('Connecting to RethinkDB');
-    r.connect({
-      host: 'localhost',
-      port: 28015,
-      db: 'classmere',
-      authKey: '',
-    }, callback);
-  },
-  function onConnection(conn, callback) {
-    console.log('Connected to RethinkDB!');
-    connection = conn;
-    callback(null);
-  },
-  function downloadCourseJSON(callback) {
-    console.log('Downloading course catalog from ' + COURSE_ENDPOINT);
-    request.get(COURSE_ENDPOINT, function handleResponse(error, res, body) {
-      if (res.statusCode !== 200) {
-        err = new Error('Got response code: ' +
-          res.statusCode +
-          ' from server');
-        callback(err);
-      } else if (error) {
-        callback(error);
-      } else {
-        console.log(body);
-        callback(null, body);
-      }
-    });
-  },
-  function parseJSON(rawJSON, callback) {
-    console.log('Parsing JSON');
-    callback(null, JSON.parse(rawJSON));
-  },
-  function insertCourses(rawParsedJSON, callback) {
-    console.log('Inserting JSON into RethinkDB');
-    r.table('rawCourseJSON')
-    .insert(rawParsedJSON)
-    .run(connection, callback);
-  },
+  downloadCourseJSON,
+  courseSchemasFromCourseJSON,
+  downloadSectionJSON,
+  insertCourses,
 ], function finished(err) {
   if (err) {
     console.error(err);
     process.exit(1);
   } else {
     console.log('Operation completed successfully');
-    connection.close()
-    .then(process.exit(0));
+    thinky.r.getPool().drain();
   }
 });
--- scraper.js
+++ scraper.js
@@ -1,16 +1,5 @@
-'use strict';
-
-const program   = require('commander');
-const request   = require('request');
-const r         = require('rethinkdb');
-const Q         = require('q');
-
-const CATALOG_API =
-'http://catalog.oregonstate.edu/Services/CatalogService.svc/rest';
-const COURSE_ENDPOINT =
-CATALOG_API + '/courses/all';
-
-let connection;
+const program       = require('commander');
+const courseJSONFetcher   = require('./rawCourseJSONFetcher');
 
 /*
  * Handle command line arguments
@@ -31,70 +20,5 @@ program
   'Print all output to the console')
 .parse(process.argv);
 
-function connectToRethink() {
-  console.log('Connecting to RethinkDB');
-
-  const deferred = Q.defer();
-  r.connect({
-    host: 'localhost',
-    port: 28015,
-    db: 'test',
-    authKey: '',
-  }, function onConnection(err, conn) {
-    if (err) {
-      deferred.reject(err);
-    } else {
-      console.log('Connected!');
-      connection = conn;
-      deferred.resolve();
-    }
-  });
-
-  return deferred.promise;
-}
-
-function downloadCourseJSON() {
-  const deferred = Q.defer();
-  console.log('Downloading course catalog from ' + COURSE_ENDPOINT);
-  request.get(COURSE_ENDPOINT, function handleResponse(error, res, body) {
-    if (res.statusCode !== 200) {
-      const err = new Error('Got response code: ' +
-        res.statusCode +
-        ' from server');
-      deferred.reject(err);
-    } else if (error) {
-      deferred.reject(error);
-    } else {
-      deferred.resolve(body);
-    }
-  });
-
-  return deferred.promise;
-}
-
-function parseJSON(rawJSON) {
-  const deferred = Q.defer();
-  console.log('Parsing JSON');
-  deferred.resolve(JSON.parse(rawJSON));
-  return deferred.promise;
-}
-
-function insertCoursesInRethink(rawParsedJSON) {
-  console.log('Inserting JSON into RethinkDB');
-
-  return Q.nfcall(
-    r.table('rawJSON')
-    .insert(rawParsedJSON)
-    .run(connection)
-  );
-}
-
-function main() {
-  Q.fcall(connectToRethink)
-  .then(downloadCourseJSON)
-  .then(parseJSON)
-  .then(insertCoursesInRethink)
-  .done();
-}
-
-main();
+// dataFormatter();
+courseJSONFetcher();
