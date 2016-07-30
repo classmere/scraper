66931e996660571699d081ffb32dbb1c732c35c6
Rutger Farry <rutgerfarry@gmail.com>
2016-07-29 20:46:21 -0700

    Remove old files

--- thinky.js
+++
@@ -1,7 +0,0 @@
-module.exports = require('thinky')({
-  host: process.env.RETHINKDB_PORT_28015_TCP_ADDR,
-  port: process.env.RETHINKDB_PORT_28015_TCP_PORT,
-  db: '',
-  authKey: '',
-  max: 10,
-});
