7f8afb5c9b3bb9e81700dd52f85b4e7103d53e9c
Rutger Farry <rutgerfarry@gmail.com>
2016-08-04 19:25:49 -0700

    Add string extensions

---
+++ stringExtensions.js
@@ -0,0 +1,13 @@
+String.prototype.stripNonAlphabetic = function() {
+  return this.replace(/[^0-9a-z]/gi, '');
+};
+
+String.prototype.stripNewlines = function() {
+  return this.replace(/\r?\n|\r/g, '');
+};
+
+String.prototype.stripExcessSpaces = function() {
+  return this.replace(/\s+/g,' ').trim();
+};
+
+module.exports = String;
