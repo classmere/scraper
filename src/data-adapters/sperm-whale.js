595992d04073a926853ecf4cd3e06a560dace394
Rutger Farry <rutgerfarry@gmail.com>
2016-08-16 21:36:23 -0700

    Versioning courses works, one bug described below.
    For some reason, some courses are having multiple versions inserted in one scrape. This shouldn’t be the case if all course urls are unique and scraped only once.
    
    Potential fix: Ensure all course urls are unique. If this doesn’t work, we can live with this problem for now

--- src/data-adapters/sperm-whale.js
+++ src/data-adapters/sperm-whale.js
@@ -1,27 +1,71 @@
 'use strict';
 
 const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const url = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 const connection = MongoClient.connect(url);
 
+/*
+ * Exports
+ */
+
 module.exports.insertCourse = function(course, cb) {
-  connection
-    .then(function(db) {
-      const courses = db.collection('courses');
-      courses.insertOne(course, (err, r) => {
-        if (err) {
-          cb(err);
-        } else {
-          cb(null, r);
-        }
-      });
+  connection.then(function(db) {
+    const courses = db.collection('courses');
+    const oldCourses = db.collection('courses.old');
+
+    courses.findOne({
+      subjectCode: course.subjectCode,
+      courseNumber: course.courseNumber,
+      title: course.title
+    }).then(function insertNewCourseOrAddVersionToOldCourseId(r) {
+      if (r === null) {
+        // Just insert the course if a previous version doesn't exist
+        course._version = 1;
+        courses.insertOne(course)
+        .then(completed);
+      } else {
+        const oldCourse = r;
+        course._version = oldCourse._version + 1;
+        oldCourse._id = {
+          _id: r._id,
+          _version: r._version
+        };
+        return oldCourse;
+      }
     })
-    .catch(function(err) {
-      process.exit(err);
-    });
+    .then(function moveOldCourseToShadowCollection(r) {
+      const oldCourse = r;
+      return oldCourses.insertOne(oldCourse);
+    })
+    .then(function deleteOldCourseFromCurrentCollection(r) {
+      assert.equal(1, r.insertedCount);
+      return courses.deleteOne({ _id: r.insertedId._id });
+    })
+    .then(function putNewCourseInCurrentCollection(r) {
+      assert.equal(1, r.deletedCount);
+      return courses.insertOne(course);
+    })
+    .then(completed)
+    .catch(errorCallback);
+  });
+
+  function completed(r) {
+    assert.equal(1, r.insertedCount);
+    cb(null, course);
+  }
 };
 
 module.exports.close = function() {
   connection.then((db) => db.close());
 };
+
+/*
+ * Internal
+ */
+
+function errorCallback(err) {
+  console.err(err);
+  process.exit(1);
+}
